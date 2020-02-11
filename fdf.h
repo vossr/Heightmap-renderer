4e6f5be3540c1d9d8f980e81f5dadf628bcbf6c1
rpehkone <pehkonenroni@gmail.com>
2020-02-11 13:10:58 +0200

    fixed norm in .h

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: rpehkone <rpehkone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 20:49:05 by rpehkone          #+#    #+#             */
-/*   Updated: 2020/02/10 17:51:16 by rpehkone         ###   ########.fr       */
+/*   Updated: 2020/02/11 13:10:19 by rpehkone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ int				handle_cursor(int x, int y, void **mlx);
 void			ft_printer(void **mlx, t_xyz *angle, t_xyz start, t_xyz stop);
 int				get_width(char *filename);
 int				get_height(char *filename);
-void			print_line(t_xyz *start, t_xyz *stop, void **mlx, unsigned color);
+void			print_line(t_xyz *start, t_xyz *stop,
+								void **mlx, unsigned color);
 int				**make_map(char *filename);
 int				fdf(int call, int x, int y, void **mlx);
 int				get_next_line(const int fd, char **line);
