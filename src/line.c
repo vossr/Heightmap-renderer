e4afd702bdc43241fee0923df1b03b4df3f2854e
rpehkone <pehkonenroni@gmail.com>
2020-03-06 23:34:26 +0200

    Line fade now works perfectly

--- src/line.c
+++ src/line.c
@@ -6,7 +6,7 @@
 /*   By: rpehkone <rpehkone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 15:19:28 by rpehkone          #+#    #+#             */
-/*   Updated: 2020/03/06 17:34:43 by rpehkone         ###   ########.fr       */
+/*   Updated: 2020/03/06 23:29:27 by rpehkone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,40 +19,59 @@ double	ft_abs(double n)
 	return (n);
 }
 
-int		make_fade(t_xyz color, unsigned fade, unsigned xred)
+int		make_fade(t_xyz color, unsigned fade, unsigned fadeout, signed xred)
 {
-	unsigned xgrn;
-	unsigned xblu;
-	unsigned yred;
-	unsigned ygrn;
-	unsigned yblu;
+	signed xgrn;
+	signed xblu;
+	signed yred;
+	signed ygrn;
+	signed yblu;
 
 	if (color.x == color.y)
 		return ((int)color.x);
 	xred = (((int)color.x % 0x1000000) >> 4 * 4) - fade;
 	xgrn = (((int)color.x % 0x10000) >> 4 * 2) - fade;
 	xblu = ((int)color.x % 0x100) - fade;
-	yred = (((int)color.y % 0x1000000) >> 4 * 4) + fade;
-	ygrn = (((int)color.y % 0x10000) >> 4 * 2) + fade;
-	yblu = ((int)color.y % 0x100) + fade;
-	xred = xred > 0 ? 0 : xred;
-	xgrn = xgrn > 0 ? 0 : xgrn;
-	xblu = xblu > 0 ? 0 : xblu;
+	yred = (((int)color.y % 0x1000000) >> 4 * 4);
+	ygrn = (((int)color.y % 0x10000) >> 4 * 2);
+	yblu = ((int)color.y % 0x100);
+	yred = (int)(((double)fade / 0xFF) * yred);
+	ygrn = (int)(((double)fade / 0xFF) * ygrn);
+	yblu = (int)(((double)fade / 0xFF) * yblu);
+	fadeout++;
+
+	xred = xred < 0 ? 0 : xred;
+	xgrn = xgrn < 0 ? 0 : xgrn;
+	xblu = xblu < 0 ? 0 : xblu;
 	yred = yred > 0xFF ? 0xFF : yred;
 	ygrn = ygrn > 0xFF ? 0xFF : ygrn;
 	yblu = yblu > 0xFF ? 0xFF : yblu;
-	/*
-	printf("xred = %d\n", xred);
-	printf("xgrn = %d\n", xgrn);
-	printf("xblu = %d\n", xblu);
-	printf("yred = %d\n", yred);
-	printf("ygrn = %d\n", ygrn);
-	printf("yblu = %d\n", yblu);
-	printf("%x\n\n", (xred << 4 * 4) + (xgrn << 4 * 2) + xblu +
-			(yred << 4 * 4) + (ygrn << 4 * 2) + yblu);
+	//printf("%d\n", 0xFF - fade);	
+	/*printf("xred = 0x%x\n", xred);
+	printf("xgrn = 0x%x\n", xgrn);
+	printf("xblu = 0x%x\n", xblu);
+	printf("yred = 0x%x\n", yred);
+	printf("ygrn = 0x%x\n", ygrn);
+	printf("yblu = 0x%x\n\n", yblu);
+	printf("fade = 0x%x\n", fade);
 	*/
+	//printf("%x\n\n\n", (xred << 4 * 4) + (xgrn << 4 * 2) + xblu +
+	//		(yred << 4 * 4) + (ygrn << 4 * 2) + yblu);
 	return ((xred << 4 * 4) + (xgrn << 4 * 2) + xblu +
 			(yred << 4 * 4) + (ygrn << 4 * 2) + yblu);
+}
+
+void	adjust_to_middle(void **mlx, int x, int y, int color)
+{
+	static int	w_move = 0;
+	static int	h_move = 0;
+
+	if (!w_move)
+	{
+		w_move = get_width(NULL) / 2;
+		h_move = get_height(NULL) / 1.3;
+	}
+	image_pixel_put(mlx, w_move + x, h_move + y, color);
 }
 
 void	print_line(t_xyz start, t_xyz stop, t_xyz color, void **mlx)
@@ -62,8 +81,6 @@ void	print_line(t_xyz start, t_xyz stop, t_xyz color, void **mlx)
 	int		i;
 
 	i = 0;
-	//color.x = 0xFF0000;
-	//color.y = 0x00FF00;
 	if (start.z < 0 || stop.z < 0)
 		return ;
 	pos.x = start.x;
@@ -75,12 +92,12 @@ void	print_line(t_xyz start, t_xyz stop, t_xyz color, void **mlx)
 	step.y = (stop.y - start.y) / (float)step.z;
 	while (pos.z <= step.z && i < 1000)
 	{
-		image_pixel_put(mlx, 500 + pos.x, 500 + pos.y,
-				make_fade(color, 0xFF * ((pos.z) / (step.z)), 0));
+		adjust_to_middle(mlx, pos.x, pos.y,
+				make_fade(color, 0xFF * ((pos.z) / (step.z)),
+					0xFF * ((step.z - pos.z) / (step.z)), 0));
 		pos.x += step.x;
 		pos.y += step.y;
 		pos.z++;
 		i++;
 	}
-	//exit(0);
 }
